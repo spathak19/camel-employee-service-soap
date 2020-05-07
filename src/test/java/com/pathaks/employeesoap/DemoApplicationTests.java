89e2133210b04a1a3c770049ae4cd825fb99f45d
spathak19 <sppathak28@gmail.com>
2020-05-07 12:57:25 +0530

    Initial Commit

---
+++ src/test/java/com/pathaks/employeesoap/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pathaks.employeesoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
