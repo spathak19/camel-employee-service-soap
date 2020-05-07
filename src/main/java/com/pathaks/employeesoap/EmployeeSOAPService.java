130eedfe38378618de8f597b8a3d376a5f3f0e22
spathak19 <sppathak28@gmail.com>
2020-05-07 14:25:21 +0530

    added db config

--- src/main/java/com/pathaks/employeesoap/EmployeeSOAPService.java
+++ src/main/java/com/pathaks/employeesoap/EmployeeSOAPService.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class EmployeeSOAPService {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(EmployeeSOAPService.class, args);
 	}
 
 }
